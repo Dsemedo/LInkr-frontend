dde7fe342c1ed5dfb230e2679d380412269df2fe
Douglas Semedo <dsemedoo@gmail.com>
2023-01-13 16:52:02 -0300

    Feat: Show posts of followed users on timelinePage

--- src/pages/User.js
+++ src/pages/User.js
@@ -16,6 +16,7 @@ export default function User() {
   const [userData, serUserData] = useState();
   const [liked, setLiked] = useState(true);
   const [followed, setFollowed] = useState(false);
+  const [usersWhoFollowed, setUsersWhoFollowed] = useState([]);
   const navigate = useNavigate();
   const params = useParams();
   const id = Number(params.id);
@@ -39,7 +40,7 @@ export default function User() {
         setHashtags(res.data);
       })
       .catch((erro) => {
-        console.log(erro);
+        console.log(erro.response);
       });
     axios
       .get(`${BASE_URL}/sign-in`, config)
@@ -51,7 +52,47 @@ export default function User() {
         localStorage.removeItem("Bearer");
         navigate("/");
       });
-  }, [navigate, id]);
+
+    axios
+      .get(`${BASE_URL}/follow/${id}`, config)
+      .then((res) => {
+        setUsersWhoFollowed(res.data);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  }, [navigate, usersWhoFollowed]);
+
+  console.log();
+
+  function followUser() {
+    let body;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("Bearer")}`,
+      },
+    };
+
+    const request = axios.post(`${BASE_URL}/follow/${id}`, body, config);
+    request.then(console.log(usersWhoFollowed)).catch((error) => {
+      console.log(error.response);
+      alert("Não foi possivel seguir esse usuário, tente novamente");
+    });
+  }
+
+  function unfollowUser() {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("Bearer")}`,
+      },
+    };
+
+    const request = axios.delete(`${BASE_URL}/follow/${id}`, config);
+    request.then(console.log(usersWhoFollowed)).catch((error) => {
+      console.log(error.response);
+      alert("Não foi possivel parar de seguir esse usuário, tente novamente");
+    });
+  }
 
   if (publishedPosts === undefined) {
     return (
@@ -75,7 +116,27 @@ export default function User() {
               userData={userData}
             />
           </TimelinePosts>
-          {/* <HashtagsBox hashtags={hashtags} /> */}
+          <RightContainer>
+            {usersWhoFollowed.followThisUser ? (
+              <Follow
+                backColor={`#ffffff`}
+                color={`#1877F2`}
+                onClick={unfollowUser}
+              >
+                Unfollow
+              </Follow>
+            ) : (
+              <Follow
+                backColor={`#1877F2`}
+                color={`#ffffff`}
+                onClick={followUser}
+              >
+                Follow
+              </Follow>
+            )}
+
+            <HashtagsBox hashtags={hashtags} />
+          </RightContainer>
         </ContainerTimeline>
       </Container>
     );
@@ -88,7 +149,7 @@ export default function User() {
     return (
       <Container onClick={() => logoutClicked && setLogoutClicked(false)}>
         <Header>
-          <h1>linkr</h1>
+          <h1 onClick={() => navigate("/timeline")}>linkr</h1>
           <Search />
           <LogoutButton
             userData={userData}
@@ -104,7 +165,27 @@ export default function User() {
             </Flex>
             <Feed>Não há Posts deste usuário</Feed>
           </TimelinePosts>
-          <HashtagsBox hashtags={hashtags} />
+          <RightContainer>
+            {usersWhoFollowed.followThisUser ? (
+              <Follow
+                backColor={`#ffffff`}
+                color={`#1877F2`}
+                onClick={unfollowUser}
+              >
+                Unfollow
+              </Follow>
+            ) : (
+              <Follow
+                backColor={`#1877F2`}
+                color={`#ffffff`}
+                onClick={followUser}
+              >
+                Follow
+              </Follow>
+            )}
+
+            <HashtagsBox hashtags={hashtags} />
+          </RightContainer>
         </ContainerTimeline>
       </Container>
     );
@@ -143,7 +224,7 @@ export default function User() {
       </Container>
     );
   }
-  if (publishedPosts !== undefined && publishedPosts.length !== 0) {
+  if (publishedPosts.length > 0) {
     return (
       <Container onClick={() => logoutClicked && setLogoutClicked(false)}>
         <Header>
@@ -170,11 +251,11 @@ export default function User() {
             />
           </TimelinePosts>
           <RightContainer>
-            {followed ? (
+            {usersWhoFollowed.followThisUser ? (
               <Follow
                 backColor={`#ffffff`}
                 color={`#1877F2`}
-                onClick={() => setFollowed(false)}
+                onClick={unfollowUser}
               >
                 Unfollow
               </Follow>
@@ -182,7 +263,7 @@ export default function User() {
               <Follow
                 backColor={`#1877F2`}
                 color={`#ffffff`}
-                onClick={() => setFollowed(true)}
+                onClick={followUser}
               >
                 Follow
               </Follow>
